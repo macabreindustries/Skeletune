720f7cf38ef1bfc068e194c9f0e3636c4fffea6e
RowerX <patinovillaaldoyahir@gmail.com>
2025-11-13 20:16:10 -0600

    segundo commit 2.0

--- skeletune/skeletune/src/main/java/com/teamllaj/skeletune/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.teamllaj.skeletune.repository;
-import com.teamllaj.skeletune.model.entity.MessageEntity; // Importación de la entidad renombrada
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface MessageRepository extends JpaRepository <MessageEntity, Long> {
-
-    // Método para buscar mensajes por nombre de usuario, ordenados por fecha descendente
-    List<MessageEntity> findByUsernameOrderBySentAtDesc(String username);
-}
